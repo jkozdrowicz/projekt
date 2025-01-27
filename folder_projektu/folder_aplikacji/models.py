957d24ddf1aeefde297376e921629a575231cd70
jkozdrowicz <jkozdrowicz1@st.swps.edu.pl>
2025-01-27 14:43:12 +0100

    Lab_09

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -1,8 +1,6 @@
-
-
 from django.db import models
-from django.utils.timezone import now
-
+from datetime import date
+from django.contrib.auth.models import User
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
@@ -33,7 +31,7 @@ class Person(models.Model):
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f"Person : {self.name}, dodana w {self.month_added}, o rozmiarze koszuli {self.shirt_size}." 
+        return f"Person : {self.name}, dodana w {self.month_added}, o rozmiarze koszuli {self.shirt_size}.\n" 
     
 class Osoba(models.Model):
     PLEC_CHOICES = (
@@ -46,13 +44,17 @@ class Osoba(models.Model):
     nazwisko = models.CharField(max_length=60, blank = False, null = False)
     plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
     stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
-    data_dodania = models.DateField(default = now)
+    data_dodania = models.DateField(default = date.today, blank=False, null=False)
+    wlasciciel = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)
     
     def __str__(self):
-        return f'{self.imie} {self.nazwisko}'
-
-    class Meta :
+        return f'{self.imie} {self.nazwisko}' 
+    
+    class Meta:
         ordering = ["nazwisko"]
+        permissions = [
+            ("view_person_other_owner", "Pozwala zobaczyć modele Osoba innych właścicieli"),
+        ]
 
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length=80, blank = False, null = False)
